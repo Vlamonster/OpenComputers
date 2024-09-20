4584f642119a7c704ff1ef768a6c1ddc12c72170
Glease <4586901+Glease@users.noreply.github.com>
2024-09-20 20:48:33 +0800

    add compat for ae2fc fluid drop content

--- src/main/scala/li/cil/oc/integration/ae2fc/ModAe2fc.scala
+++ src/main/scala/li/cil/oc/integration/ae2fc/ModAe2fc.scala
@@ -1,8 +1,11 @@
 package li.cil.oc.integration.ae2fc
 
+import li.cil.oc.api.Driver
 import li.cil.oc.integration.{Mod, ModProxy, Mods}
 
 object ModAe2fc  extends ModProxy {
   override def getMod: Mod = Mods.Ae2Fc
-  override def initialize(): Unit = {}
+  override def initialize(): Unit = {
+    Driver.add(ConverterFluidDrop)
+  }
 }
